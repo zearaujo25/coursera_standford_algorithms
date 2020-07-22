f012df52a7269c1fbc46f488748ab5f22aef11f0
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-07-21 21:51:57 -0300

    Inclusion of new test for recursive multiplication

--- divide_and_conquer/week2/matrix_multiplication/recusrsive/recusrsvive_tests.py
+++ divide_and_conquer/week2/matrix_multiplication/recusrsive/recusrsvive_tests.py
@@ -5,6 +5,7 @@ def main():
     test_add()
     test_multiply_identities()
     test_multiply_random_matrix_with_identity()
+    test_multiply_fixed_input()
     print("ALL TESTS PASSED")
 
 def test_add():
@@ -34,6 +35,29 @@ def test_multiply_random_matrix_with_identity():
     expected_result =  random_matrix
     assert result == expected_result, "Wrong result-- Expected: {}, result: {}".format(expected_result,result)
     print("TEST MULTIPLY_RANDOM PASSED")
+
+
+def test_multiply_fixed_input():
+    print("TEST MULTIPLY_FIXED START") 
+    matrix_x = [ 
+               [1,2,3,4],
+               [5,6,7,8],
+               [9,10,11,12],
+               [13,14,15,16] 
+               ]
+    matrix_y = [ [13,14,15,16],
+               [9,10,11,12],
+               [5,6,7,8],
+               [1,2,3,4]              
+                ]
+    expected_result = [ [50,60,70,80],
+                        [162,188,214,240],
+                        [274,316,358,400],
+                        [386,444,502,560]]   
+    result = multiply(matrix_x,matrix_y)
+    assert result == expected_result, "Wrong result-- Expected: {}, result: {}".format(expected_result,result)
+    print("TEST MULTIPLY_FIXED PASSED")
+
 
 def create_indentity(n):
     return [[1 if i==j else 0 for j in range(0,n) ] for i in range(0,n) ]
