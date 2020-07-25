c64cf7e1495341b4a2a661c48962f8eb660dcd25
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-07-24 23:56:36 -0300

    Notes

--- divide_and_conquer/week2/closest_points/closest_points.py
+++ divide_and_conquer/week2/closest_points/closest_points.py
@@ -5,6 +5,7 @@ def pre_process(points):
     return merge_sort(points,0), merge_sort(points,1)
 
 def find_closest(points = None,ordered_points_by_x=None,ordered_points_by_y=None):
+    #remeber, they are the same set of points ordered by differetn criteria
 
     if points is not None:
         ordered_points_by_x,ordered_points_by_y = pre_process(points)
