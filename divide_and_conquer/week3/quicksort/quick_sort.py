12d94bf65ae8bce6d94a16b0868672c0a51ce715
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-08-02 22:34:45 -0300

    Finishing quick sort assignment

--- divide_and_conquer/week3/quicksort/quick_sort.py
+++ divide_and_conquer/week3/quicksort/quick_sort.py
@@ -1,17 +1,50 @@
-def quick_sort(array, left_end=None,right_end=None):
+def quick_sort(array, left_end=None, right_end=None, pivot_method='naive_begin' ):
+    
     if(left_end is None or right_end is None):
             left_end = 0
             right_end = len(array) -1
+    number_of_comparisons = get_number_of_comparisons(array,left_end,right_end)
     if left_end >= right_end:
-        return
-    pivot_position = choose_pivot(array,left_end,right_end)
-    swap(array,left_end,pivot_position)
-    pivot_final_position = partition(array,left_end,right_end)
-    quick_sort(array,left_end,pivot_final_position-1)
-    quick_sort(array,pivot_final_position+1,right_end)
+        return number_of_comparisons
 
-def choose_pivot(array,left_end,right_end):
+    pivot_position = get_pivot(pivot_method, array, left_end, right_end)
+    swap(array,left_end,pivot_position)
+    
+    pivot_final_position = partition(array,left_end,right_end)
+    
+    number_of_comparisons = get_number_of_comparisons(array,left_end,right_end)
+    left = quick_sort(array,left_end,pivot_final_position-1,pivot_method=pivot_method)
+    right = quick_sort(array,pivot_final_position+1,right_end,pivot_method)
+    return left + right + number_of_comparisons
+
+def get_pivot(pivot_method, array, left_end, right_end):
+
+    if pivot_method == 'naive_begin':
+        return choose_pivot_naive_begin(array,left_end,right_end)
+    elif pivot_method == 'naive_end':
+        return choose_pivot_naive_end(array,left_end,right_end)
+    elif pivot_method == 'median': 
+        return  choose_pivot_median(array,left_end,right_end)
+    else:
+        raise Exception("Wrong pivot method")
+
+def choose_pivot_naive_begin(array,left_end,right_end):
     return left_end
+
+def choose_pivot_naive_end(array,left_end,right_end):
+    return right_end
+
+def choose_pivot_median(array,left_end,right_end):
+    middle = left_end + ((right_end-left_end)//2) 
+    aux_array = [array[left_end],array[middle],array[right_end]]
+    aux_array.sort()
+    median = aux_array[1]
+    if median == array[left_end]:
+        return left_end
+    elif median == array[right_end]:
+        return right_end
+    else:
+        return middle
 
 def partition(array,left_end,right_end):
     pivot = array[left_end]
@@ -20,6 +53,7 @@ def partition(array,left_end,right_end):
         if array[j]<pivot:
             swap(array,i,j)
             i += 1 
+    
     swap(array,left_end,i-1)
     #final place of the pivot 
     return i-1
@@ -27,12 +61,22 @@ def partition(array,left_end,right_end):
 def swap(array,i,j):
     array[i],array[j] = array[j],array[i]
 
-def main():
-    sorted_array = [0,1,2,3,4,5,6,7,8,9]
-    array = [9,8,7,6,5,4,3,2,1,0]
-    quick_sort(array)
-    assert sorted_array == array , "Array not properly sorted: {}".format(array)
+def get_number_of_comparisons(array,left_end,right_end):
+    return  len(array[left_end:right_end]) if left_end < right_end else 0
 
+#Assignment code
+def read_txt(path):
+    test_case = []
+    with open(path) as f: 
+        for line in f: 
+            test_case.append(int(line))
+    return test_case
+
+def main():
+    assignment = read_txt("assignment.txt")
+    # print("Assignment naive begin: {}".format(quick_sort(assignment,pivot_method="naive_begin")))
+    # print("Assignment naive end: {}".format(quick_sort(assignment,pivot_method="naive_end")))
+    print("Assignment naive median: {}".format(quick_sort(assignment,pivot_method="median")))
 
 if __name__ == "__main__":
     main()
--- divide_and_conquer/week3/quicksort/quick_sort_test.py
+++ divide_and_conquer/week3/quicksort/quick_sort_test.py
@@ -1,4 +1,5 @@
 from quick_sort import quick_sort
+from quick_sort import choose_pivot_median,get_number_of_comparisons
 
 
 import unittest
@@ -9,19 +10,50 @@ class QuickSortTest(unittest.TestCase):
         test_case = [9,8,7,6,5,4,3,2,1,0]
         ordered = [0,1,2,3,4,5,6,7,8,9]
         quick_sort(test_case)
-        self.assertEqual(ordered,test_case,"Array not sorted: {}".format(test_case)) 
+        self.assertEqual(ordered,test_case,"Array not sorted") 
 
     def ordered_test(self):
         test_case = [0,1,2,3,4,5,6,7,8,9]
         ordered = [0,1,2,3,4,5,6,7,8,9]
         quick_sort(test_case)
-        self.assertEqual(ordered,test_case,"Array not sorted: {}".format(test_case)) 
+        self.assertEqual(ordered,test_case,"Array not sorted") 
 
     def partially_ordered_test(self):
         test_case = [5,6,7,8,9,0,1,2,3,4]
         ordered = [0,1,2,3,4,5,6,7,8,9]
         quick_sort(test_case)
-        self.assertEqual(ordered,test_case,"Array not sorted: {}".format(test_case)) 
+        self.assertEqual(ordered,test_case,"Array not sorted") 
+
+    def test_order_median_method(self):
+        test_case = [2, 20, 1, 15, 3, 11, 13, 6, 16, 10, 19, 5, 4, 9, 8, 14, 18, 17, 7, 12] 
+
+        ordered = [2, 20, 1, 15, 3, 11, 13, 6, 16, 10, 19, 5, 4, 9, 8, 14, 18, 17, 7, 12]
+        ordered.sort()
+        true_count_number = 55
+        test_count_number = quick_sort(test_case,pivot_method="median")
+
+        self.assertEqual(ordered,test_case,"Array not sorted") 
+        self.assertEqual(true_count_number,test_count_number,"Wrong number of counts: {}".format(test_count_number)) 
+
+    def test_median_pivot_method(self):
+       
+        test_single_median_pivot(self,[2, 20, 1, 15, 3, 11, 13, 6, 16, 10, 19, 5, 4, 9, 8, 14, 18, 17, 7, 12],10 ) 
+        test_single_median_pivot(self,[7, 1, 3, 6, 2, 5, 4, 9, 8] ,7 ) 
+        test_single_median_pivot(self,[4, 1, 3, 6, 2, 5] ,4 ) 
+        test_single_median_pivot(self,[2, 1, 3], 2) 
+        test_single_median_pivot(self,[6, 5] , 6) 
+    
+    def test_get_number_of_comparisons(self):
+        pass
+
+def test_single_median_pivot(test,test_case,correct_median):
+
+    median_index = choose_pivot_median(test_case,0,len(test_case)-1)
+    median_test = test_case[median_index]
+
+    test.assertEqual(correct_median,median_test,"Wrong median: {}".format(median_test)) 
+    
+    
 
 
 if __name__ == '__main__':
