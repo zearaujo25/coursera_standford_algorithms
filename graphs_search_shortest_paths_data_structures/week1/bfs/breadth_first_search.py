205ee06c6ae92f7a03b76ae934185237869eb0a6
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-08-15 19:55:51 -0300

    adding connected componets in breadth first search

--- graphs_search_shortest_paths_data_structures/week1/bfs/breadth_first_search.py
+++ graphs_search_shortest_paths_data_structures/week1/bfs/breadth_first_search.py
@@ -1,6 +1,7 @@
 from queue import Queue
 def breadth_first_search(graph,node):
     node.mark_as_explored()
+    node.set_distance(0)
     search_queue = Queue()
     search_queue.put(node)
     while not search_queue.empty():
@@ -9,5 +10,13 @@ def breadth_first_search(graph,node):
             destination = edge.get_destination(next_node)
             if not destination.is_explored():
                 destination.mark_as_explored()
+                destination.set_distance(next_node.get_distance() +1)
                 search_queue.put(destination)
 
+def find_connected_components(graph):
+    number_of_conections = 0
+    for node in graph.get_nodes():
+        if not node.is_explored():
+            number_of_conections+=1
+            breadth_first_search(graph,node)
+    return number_of_conections
--- graphs_search_shortest_paths_data_structures/week1/bfs/breadth_first_search_test.py
+++ graphs_search_shortest_paths_data_structures/week1/bfs/breadth_first_search_test.py
@@ -2,7 +2,7 @@ import unittest
 from graph import Graph
 from node import Node
 from edge import Edge
-from breadth_first_search import breadth_first_search
+from breadth_first_search import breadth_first_search,find_connected_components
 
 class EdgeTest(unittest.TestCase):
     def breadth_first_search_undirected_connected_graph_test(self):
@@ -20,6 +20,8 @@ class EdgeTest(unittest.TestCase):
         breadth_first_search(graph,origin_node)
         for node in test_nodes:
             self.assertTrue(node.is_explored())
+            self.assertEquals(1,node)
+
     def breadth_first_search_undirected_disconnected_graph_test(self):
         graph = Graph()
         test_nodes = [Node(node_id=0),Node(node_id=1),Node(node_id=2),Node(node_id=3)]
@@ -34,8 +36,36 @@ class EdgeTest(unittest.TestCase):
         breadth_first_search(graph,origin_node)
         
         self.assertFalse(test_nodes[3].is_explored())
+        self.assertEquals(float("inf"),test_nodes[3].get_distance())
         for node in test_nodes[:3]:
             self.assertTrue(node.is_explored())
+            self.assertEquals(1,node)
+
+    def find_connected_components_test(self):
+        graph = Graph()
+        #Graph from https://d3c33hcgiwev3.cloudfront.net/_bc5d400c4401bb47019da53961937cea_slides_algo-graphs-bfs_typed.pdf?Expires=1597622400&Signature=SsHqQKe-GdB6sewmyOL9GC2KrOKfPF3qAzMW8TBOGboyWV9WgRWgnBEaYO5dYcTxMUmNf2bTL2cTuivPXePAZ0KwAyUGTHxy2SFCwt1pdz8MQ5GuMnu0opPpVxEdvsnrYdDVgjd3zsJGJY7Q0tymCr7TmPebMbhwv9dpR6jAELY_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A
+        test_nodes =[Node(node_id=i) for i in range(10) ] 
+        
+        connection1 = [Edge(test_nodes[0],test_nodes[2]),
+                      Edge(test_nodes[0],test_nodes[4]),
+                      Edge(test_nodes[2],test_nodes[4]),
+                      Edge(test_nodes[4],test_nodes[8]),
+                      Edge(test_nodes[4],test_nodes[6])]
+
+        connection2 = [Edge(test_nodes[1],test_nodes[3])]
+
+        connection3 = [Edge(test_nodes[5],test_nodes[7]),
+                      Edge(test_nodes[5],test_nodes[9]),
+                      Edge(test_nodes[7],test_nodes[9])]
+
+        graph.add_nodes(test_nodes)
+        graph.add_edges(connection1)
+        graph.add_edges(connection2)
+        graph.add_edges(connection3)
+
+        test_case = find_connected_components(graph)
+        
+        self.assertEquals(3,test_case)
 
 if __name__ == "__main__":
     unittest.main()
