446b737f74f1cb628f2508736adc28d748ab2a65
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-08-29 23:00:19 -0300

    adding big arrays test

--- graphs_search_shortest_paths_data_structures/week3/heap/test_heap.py
+++ graphs_search_shortest_paths_data_structures/week3/heap/test_heap.py
@@ -1,6 +1,7 @@
 import unittest
+from random import shuffle,seed
 from heap import heap_insert,heap_pop
-
+seed(42)
 
 class TestHeap(unittest.TestCase):
     def test_min_heap_insert(self):
@@ -8,6 +9,7 @@ class TestHeap(unittest.TestCase):
         test_heap = []
         test_position_map = {}
         heap_type = 'min'
+        
         for test_element in test_case:
             heap_insert(test_heap,test_position_map,test_element,heap_type)
             self.assertEqual(test_element,test_heap[0])
@@ -20,12 +22,30 @@ class TestHeap(unittest.TestCase):
         heap_type = 'min'
         correct_order = [i for i in range(1,11)]
         test_answer = []
+        
         for test_element in test_case:
             heap_insert(test_heap,test_position_map,test_element,heap_type)
         
         while len(test_heap) > 0:
             test_answer.append(heap_pop(test_heap,test_position_map))
         self.assertEqual(correct_order,test_answer)
+
+    def test_heap_extract_min_big_random_array(self):
+        array_size = 100000
+        test_case = [i for i in range(array_size,0,-1)]
+        shuffle(test_case)
+        test_heap = []
+        test_position_map = {}
+        heap_type = 'min'
+        correct_order = [i for i in range(1,array_size+1)]
+        test_answer = []
+        
+        for test_element in test_case:
+            heap_insert(test_heap,test_position_map,test_element,heap_type)
+        
+        while len(test_heap) > 0:
+            test_answer.append(heap_pop(test_heap,test_position_map,heap_type))
+        self.assertEqual(correct_order,test_answer) 
 
     def test_max_heap_insert(self):
         test_case = [i for i in range(1,11)]
@@ -50,6 +70,22 @@ class TestHeap(unittest.TestCase):
         while len(test_heap) > 0:
             test_answer.append(heap_pop(test_heap,test_position_map,heap_type))
         self.assertEqual(correct_order,test_answer)
+
+    def test_heap_extract_max_big_random_array(self):
+        array_size = 100000
+        test_case = [i for i in range(1,array_size+1)]
+        shuffle(test_case)
+        test_heap = []
+        test_position_map = {}
+        heap_type = 'max'
+        correct_order = [i for i in range(array_size,0,-1) ]
+        test_answer = []
+        for test_element in test_case:
+            heap_insert(test_heap,test_position_map,test_element,heap_type)
+        
+        while len(test_heap) > 0:
+            test_answer.append(heap_pop(test_heap,test_position_map,heap_type))
+        self.assertEqual(correct_order,test_answer) 
 
 if __name__ == "__main__":
     unittest.main()
