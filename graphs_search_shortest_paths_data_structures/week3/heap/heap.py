0ed19b6279a80792f5d13de072dad20bbf3f9506
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-08-29 22:40:45 -0300

    changing heap to support both min and max

--- graphs_search_shortest_paths_data_structures/week3/heap/heap.py
+++ graphs_search_shortest_paths_data_structures/week3/heap/heap.py
@@ -1,13 +1,15 @@
-def heap_insert(heap,position_map,element):
+def heap_insert(heap,position_map,element,heap_type='min'):
     heap.append(element)
     position_map[element] = len(heap)-1
-    bubble_up(heap,position_map,element)
+    bubble_up(heap,position_map,element,heap_type)
 
-def bubble_up(heap,position_map,element):
+def bubble_up(heap,position_map,element,heap_type='min'):
     parent = heap[(position_map[element]+1)//2 -1]
-    while element < parent and position_map[element] != 0:
+    should_swap =  element < parent if heap_type == 'min' else element > parent
+    while should_swap and position_map[element] != 0:
         swap_elements(heap,position_map,(element,parent))
         parent = heap[(position_map[element]+1)//2 -1]
+        should_swap =  element < parent if heap_type == 'min' else element > parent
 
 def swap_elements(heap,position_map,elements):
     positions = position_map[elements[0]],position_map[elements[1]]
@@ -19,24 +21,31 @@ def swap_elements(heap,position_map,elements):
     position_map[elements[1]] = positions[0]
 
 
-def heap_extract_min(heap,position_map):
+def heap_pop(heap,position_map,heap_type='min'):
     swap_elements(heap,position_map,(heap[0],heap[-1]))
-    min_element = heap.pop()
-    position_map.pop(min_element)
+    popped_element = heap.pop()
+    position_map.pop(popped_element)
     if len(heap) >1:
-        bubble_down(heap,position_map,heap[0])
-    return min_element
+        bubble_down(heap,position_map,heap[0],heap_type)
+    return popped_element
 
-def bubble_down(heap,position_map,element):
-    min_child = get_min_child(position_map, element, heap)
-    while element > min_child:
-        swap_elements(heap,position_map,(element,min_child))
-        min_child = get_min_child(position_map, element, heap)
+def bubble_down(heap,position_map,element,heap_type='min'):
+    child_to_swap = get_swap_child(position_map, element, heap,heap_type)
+    should_swap =  element > child_to_swap if heap_type == 'min' else element < child_to_swap
+    while should_swap:
+        swap_elements(heap,position_map,(element,child_to_swap))
+        child_to_swap = get_swap_child(position_map, element, heap)
+        should_swap =  element > child_to_swap if heap_type == 'min' else element < child_to_swap
 
-def get_min_child(position_map, element, heap):
-    left_child = heap[(position_map[element]+1)*2-1] if (position_map[element]+1)*2-1 < len(heap) else float("inf")
-    right_child = heap[(position_map[element]+1)*2] if (position_map[element]+1)*2 < len(heap) else float("inf")
-    return min(left_child,right_child)
+def get_swap_child(position_map, element, heap,heap_type='min'):
+    if heap_type == 'min':
+        left_child = heap[(position_map[element]+1)*2-1] if (position_map[element]+1)*2-1 < len(heap) else float("inf")
+        right_child = heap[(position_map[element]+1)*2] if (position_map[element]+1)*2 < len(heap) else float("inf")
+        return min(left_child,right_child)
+    else:
+        left_child = heap[(position_map[element]+1)*2-1] if (position_map[element]+1)*2-1 < len(heap) else float("-inf")
+        right_child = heap[(position_map[element]+1)*2] if (position_map[element]+1)*2 < len(heap) else float("-inf")
+        return max(left_child,right_child)
 
 def heap_delete(heap,position_map,element):
     element_position = position_map[element]
--- graphs_search_shortest_paths_data_structures/week3/heap/test_heap.py
+++ graphs_search_shortest_paths_data_structures/week3/heap/test_heap.py
@@ -1,15 +1,15 @@
 import unittest
-from heap import heap_insert,heap_extract_min
+from heap import heap_insert,heap_pop
 
 
 class TestHeap(unittest.TestCase):
-    def test_heap_insert(self):
+    def test_min_heap_insert(self):
         test_case = [i for i in range(10,0,-1)]
         test_heap = []
         test_position_map = {}
-
+        heap_type = 'min'
         for test_element in test_case:
-            heap_insert(test_heap,test_position_map,test_element)
+            heap_insert(test_heap,test_position_map,test_element,heap_type)
             self.assertEqual(test_element,test_heap[0])
         self.assertEqual(len(test_case),len(test_heap))
 
@@ -17,14 +17,15 @@ class TestHeap(unittest.TestCase):
         test_case = [i for i in range(10,0,-1)]
         test_heap = []
         test_position_map = {}
+        heap_type = 'min'
         correct_order = [i for i in range(1,11)]
         test_answer = []
         for test_element in test_case:
-            heap_insert(test_heap,test_position_map,test_element)
+            heap_insert(test_heap,test_position_map,test_element,heap_type)
         
         while len(test_heap) > 0:
-            test_answer.append(heap_extract_min(test_heap,test_position_map))
+            test_answer.append(heap_pop(test_heap,test_position_map))
         self.assertEqual(correct_order,test_answer)
-        
+
 if __name__ == "__main__":
     unittest.main()
