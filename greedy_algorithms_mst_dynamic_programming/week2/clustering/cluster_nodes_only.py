48aacf8baf9dd49d7c656494c325f358cde1a71a
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-09-20 14:11:01 -0300

    fxing condition loop

--- greedy_algorithms_mst_dynamic_programming/week2/clustering/cluster_nodes_only.py
+++ greedy_algorithms_mst_dynamic_programming/week2/clustering/cluster_nodes_only.py
@@ -2,18 +2,17 @@ from union_find import UnionFind
 
 class ClusterNodesOnly():
     def __init__(self):
-        self.nodes = set()
-        self.main_union_find = UnionFind()
+        self.union_find = UnionFind()
         self.one_distances = []
         self.two_distances = []
 
     def find_all_one_distances(self,node):
         possible_nodes = []
         for bit in range(0,len(node)):
-            search_node = node
+            search_node = node.copy()
             bit_to_change = "0" if node[bit] == '1' else "1"
             search_node[bit] = bit_to_change
-            if search_node in self.nodes:
+            if tuple(search_node) in self.union_find.node_leaders:
                 possible_nodes.append(search_node)
 
         return possible_nodes
@@ -22,19 +21,18 @@ class ClusterNodesOnly():
         possible_nodes = []
         for first_bit in range(0,len(node)-1):
             for second_bit in range(1,len(node)):
-                search_node = node
+                search_node = node.copy()
                 first_bit_to_change = "0" if node[first_bit] == "1" else "1"
                 second_bit_to_change = "0" if node[second_bit] == "1" else "1"
                 search_node[first_bit] = first_bit_to_change
                 search_node[second_bit] = second_bit_to_change
-                if search_node in self.nodes:
+                if tuple(search_node) in self.union_find.node_leaders:
                     possible_nodes.append(search_node)
 
         return possible_nodes
 
     def add_node(self,node):
-        self.nodes.add(node)
-        self.main_union_find.add_element(node)
+        self.union_find.add_element(tuple(node))
         one_distances = self.find_all_one_distances(node)
         for close_node in one_distances:
             self.one_distances.append((node,close_node))
@@ -46,13 +44,13 @@ class ClusterNodesOnly():
 
 
     def clusterfy(self):
-        while len(self.one_distances) > 0 and len(self.two_distances) > 0:
+        while len(self.one_distances) > 0 or len(self.two_distances) > 0:
             nodes = self.get_next_pair_of_nodes()
-            leader1 = self.main_union_find.find(nodes[0])
-            leader2 = self.main_union_find.find(nodes[1])
+            leader1 = self.union_find.find(tuple(nodes[0]))
+            leader2 = self.union_find.find(tuple(nodes[1]))
             if leader1 != leader2:
-                self.main_union_find.union(leader1,leader2)
-            return len(self.main_union_find.clusters)
+                self.union_find.union(tuple(leader1),tuple(leader2))
+        return len(self.union_find.clusters)
 
 
     def get_next_pair_of_nodes(self):
