21223378ec606f3cf5e3c75686351e8a554f18f2
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-09-20 00:32:34 -0300

    fixing alogrythm

--- greedy_algorithms_mst_dynamic_programming/week2/clustering/cluster_graph.py
+++ greedy_algorithms_mst_dynamic_programming/week2/clustering/cluster_graph.py
@@ -10,11 +10,8 @@ class ClusterGraph():
         self.union_find.add_element(edge[0])
         self.union_find.add_element(edge[1])
 
-    def sort_edges(self):
-        self.edges.sort(key= lambda edge: edge[2])
 
-
-    def clusterfy(self,edges,num_clusters=4):
+    def clusterfy(self,num_clusters=4):
         self.sort_edges()
         while len(self.union_find.clusters) > num_clusters:
             edge = self.edges.pop()
@@ -22,6 +19,17 @@ class ClusterGraph():
             leader2 = self.union_find.find(edge[1])
             if leader1 != leader2:
                 self.union_find.union(leader1,leader2)
+
+    def sort_edges(self):
+        self.edges.sort(key= lambda edge: edge[2],reverse=True)
+
     def get_maximum_spacing(self):
-        return max(self.edges,key= lambda edge: edge[2])
+        final_edges = []
+        for edge in self.edges:
+            leader1 = self.union_find.find(edge[0])
+            leader2 = self.union_find.find(edge[1])
+            if leader1 != leader2:
+                final_edges.append(edge)
+
+        return min(final_edges,key= lambda edge: edge[2])[2]
     
