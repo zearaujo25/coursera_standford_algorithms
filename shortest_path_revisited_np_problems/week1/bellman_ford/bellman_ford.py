5e7e83682e21b4df5dbb168da49f401d961f5ad2
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-10-11 16:53:36 -0300

    inclusion of bellman ford

---
+++ shortest_path_revisited_np_problems/week1/bellman_ford/bellman_ford.py
@@ -0,0 +1,35 @@
+from collections import deque
+def bellman_shortest_path(graph,source):
+    solution_array = deque()
+    node_len = len(graph)
+    create_base_cases(source, graph, solution_array)
+    num_edges = 1 
+    while num_edges < node_len:
+        find_iteration_solution(graph, solution_array)
+        #optimizing space by getting rid of unecessary solutions
+        solution_array.popleft()
+        num_edges+=1
+    find_iteration_solution(graph,solution_array)
+    return None if solution_array[-1] != solution_array[0] else solution_array[0]
+
+
+def create_base_cases(source, graph, solution_array):
+    base_case = {}
+    base_case[source] = 0
+    for node in graph.keys():
+        if node != source:
+            base_case[node] = float("inf")
+    solution_array.append(base_case)
+    
+
+def find_iteration_solution(graph, solution_array):
+    iteration_solution = {}
+    for node in graph:
+        min_path_next_node = min([ solution_array[-1][incoming_edge[0]] + incoming_edge[2] for incoming_edge in graph[node]['incomming_edges']]) if len(graph[node]['incomming_edges']) >0 else float("inf")
+        iteration_solution[node] = min(min_path_next_node,solution_array[-1][node])
+    solution_array.append(iteration_solution)
+    
+
+
+    
+
---
+++ shortest_path_revisited_np_problems/week1/bellman_ford/test_bellman_ford.py
@@ -0,0 +1,53 @@
+import unittest
+import os
+from bellman_ford import bellman_shortest_path
+
+def read_graph(graph_path,sep=" "):
+    graph = {}
+    line_number = 0
+    with open(graph_path) as f: 
+        for line in f: 
+            split_line = line.strip("\n").split(sep=sep)
+            if line_number == 0:
+                for node in range(1,int(split_line[0])+1):
+                    graph[node] = {'incomming_edges':set(),'outgoing_edges':set()}
+                line_number+=1
+                continue
+            node_out = int(split_line[0])
+            node_in = int(split_line[1])
+            edge_weight = int(split_line[2])
+            graph[node_out]['outgoing_edges'].add((node_out,node_in,edge_weight))
+            graph[node_in]['incomming_edges'].add((node_out,node_in,edge_weight))
+            line_number+=1
+    return graph
+
+def get_assignment_answer(test_distance_result):
+    nodes = [7,37,59,82,99,115,133,165,188,197]
+    return [test_distance_result[node] for node in nodes]
+
+def get_test_inputs(path):
+    inputs = []
+    for file in os.listdir(path):
+        if  "input" in file:
+            inputs.append(os.path.join(path, file))
+    return inputs
+
+def read_output(test_case_path):
+    with open(test_case_path.replace("input","output")) as f: 
+       for line in f: 
+           return [int(leader) for leader in line.split(',')]
+
+
+
+class TestBellmanFord(unittest.TestCase):
+    def test_negative_cicle(self):
+        test_case = read_graph("shortest_path_revisited_np_problems/week1/test_cases/input_random_1_2.txt"," ")
+        result_distances = bellman_shortest_path(test_case,1)
+        self.assertEqual(None,result_distances)
+    def test_small_case(self):
+        test_case = read_graph("shortest_path_revisited_np_problems/week1/test_cases/input_random_2_2.txt"," ")
+        result_distances = bellman_shortest_path(test_case,1)
+        self.assertEqual(-66,min(result_distances.values()))
+
+if __name__ == "__main__":
+    unittest.main()
